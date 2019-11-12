8142722cf1fa510665e76f0babd37ce7dfbbecc9
kyeung7 <kevin.yeung.1@uvm.edu>
2019-11-12 16:04:14 -0500

    Initial Commit - KY

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Homebrew, November 12, 2019 [Kevin Yeung, Jordan Deso, Conner Burleson]
+/* This program is the main... */
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello user! This is the major program requirement.\n" << std::endl;
+}
+
+int main()
+{
+    hello();
+
+
+
+}
